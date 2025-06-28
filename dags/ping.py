c99713650649e328a86bb49185c386b129dcf807
deathstarset <anisbaziz.sub@gmail.com>
2025-06-29 00:30:23 +0100

    Solve airflow bugs

--- dags/ping.py
+++ dags/ping.py
@@ -6,20 +6,20 @@ def say_hello():
     print("👋 Hello from Airflow!")
 
 default_args = {
-    'owner': 'test',
+    'owner': 'anis',
     'retries': 0,
     'retry_delay': timedelta(minutes=1),
 }
 
 with DAG(
-    dag_id='test_hello_airflow',
+    dag_id='hello_airflow',
     default_args=default_args,
     description='A simple test DAG',
-    schedule_interval=None,  # Only run manually
+    schedule_interval=None,
     start_date=datetime(2024, 1, 1),
     catchup=False,
 ) as dag:
-    hello_task = PythonOperator(
-        task_id='say_hello_task',
+    ping_task = PythonOperator(
+        task_id='ping_task',
         python_callable=say_hello,
     )
