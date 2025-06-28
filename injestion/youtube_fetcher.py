daf139c60544f8a2f7214bc0a0c27df2012cd80c
deathstarset <anisbaziz.sub@gmail.com>
2025-06-28 21:52:33 +0100

    Create youtube fetcher class

--- injestion/youtube_fetcher.py
+++ injestion/youtube_fetcher.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 from googleapiclient.discovery import build
 import pandas as pd
 from datetime import datetime, timezone
+import isodate
+
 
 load_dotenv()
 
@@ -17,6 +19,76 @@ class YouTubeTrendingFetcher:
         self.youtube = build("youtube", "v3", developerKey=self.api_key)
         
     
+
+    def parse_duration(self, duration_str: str) -> float:
+        """ Parses duration from string format to a normal float
+
+        Args:
+            - duration_str: duration string
+
+        Returns: Parsed duration float
+        """
+        try:
+            return isodate.parse_duration(duration_str).total_seconds()
+        except Exception:
+            return None
+        
+    def get_category_mapping(self, region: str) -> dict:
+        """ Gets category mapping based on a certain region
+
+        Args:
+            region: Region string
+        
+        Returns: Category mapping dictionnary
+        """
+
+        request = self.youtube.videoCategories().list(
+        part="snippet",
+        regionCode=region
+        )
+        response = request.execute()
+
+        return {item["id"]: item["snippet"]["title"] for item in response.get("items", [])}
+
+    def get_channel_details(self, channel_ids: list[str]) -> pd.DataFrame:
+        """ Get channels based on channel_id 
+        
+        Args:
+            channel_ids: list of channel ids
+        
+        Returns: Dataframe of channels details
+        
+        """
+
+        channel_data = []
+
+
+        # Process channels in a batch of 50
+        for i in range(0, len(channel_ids), 50):
+            # Fetch data for 50 channels
+            ids_chunk = channel_ids[i:i+50]
+            request = self.youtube.channels().list(
+            part="snippet,statistics",
+            id=",".join(ids_chunk)
+            )
+            response = request.execute()
+
+            # Add each channel data
+            for item in response.get("items", []):
+                channel = {
+                    "channel_id": item["id"],
+                    "channel_title": item["snippet"]["title"],
+                    "subscriber_count": int(item["statistics"].get("subscriberCount", 0)),
+                    "total_views": int(item["statistics"].get("viewCount", 0)),
+                    "channel_created_at": item["snippet"]["publishedAt"],
+                    "channel_country": item["snippet"].get("country", None)
+                }
+                channel_data.append(channel)
+
+        return pd.DataFrame(channel_data)
+
+
+
     def fetch_trending_videos(self, region: str):
         """ Fetches trending videos based on a certain region
         
@@ -48,7 +120,7 @@ class YouTubeTrendingFetcher:
                 "view_count": int(item["statistics"].get("viewCount", 0)),
                 "like_count": int(item["statistics"].get("likeCount", 0)),
                 "comment_count": int(item["statistics"].get("commentCount", 0)),
-                "duration_seconds": item["contentDetails"]["duration"],
+                "duration_seconds": self.parse_duration(item["contentDetails"]["duration"]),
                 "region_code": region,
                 "ingested_at": datetime.now(timezone.utc)
             }
@@ -59,11 +131,26 @@ class YouTubeTrendingFetcher:
     
     def run(self):
         """ Performs fetching for the regions """
-
         all_videos = []
         for region in self.regions:
+            # Fetch trending videos
             print(f"Fetching trending videos for region: {region}")
             df_videos = self.fetch_trending_videos(region)
+
+            # Add category names
+            cat_map = self.get_category_mapping(region)
+            df_videos["category_name"] = df_videos["category_id"].map(cat_map)
+
+            # Add channel info:
+            unique_channels = df_videos["channel_id"].unique().tolist()
+            df_channels = self.get_channel_details(unique_channels)
+            df_videos = pd.merge(df_videos, df_channels, on="channel_id", how="left")
+
+            # Clean duplicates
+            df_videos.drop_duplicates(subset=["video_id", "region_code", "ingested_at"], inplace=True)
+
+            # Add videos to videos list
             all_videos.append(df_videos)
+
 
         return pd.concat(all_videos, ignore_index=True)
